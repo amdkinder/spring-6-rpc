081c75916b796e32a053f49ba7e3c46f275f4b70
Kuchkaroff <akhrorbek.kuchkarov@gmail.com>
2023-01-26 21:10:05 +0500

    init

---
+++ src/main/kotlin/uz/anorbank/spring6rpc/Spring6RpcApplication.kt
@@ -0,0 +1,11 @@
+package uz.anorbank.spring6rpc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring6RpcApplication
+
+fun main(args: Array<String>) {
+    runApplication<Spring6RpcApplication>(*args)
+}
---
+++ src/test/kotlin/uz/anorbank/spring6rpc/Spring6RpcApplicationTests.kt
@@ -0,0 +1,13 @@
+package uz.anorbank.spring6rpc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Spring6RpcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
